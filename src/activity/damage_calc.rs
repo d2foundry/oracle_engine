46672aad96342f4ed812b299b272b50a2f114da5
Harmon Brammer <harm@brammer.dev>
2023-05-18 18:50:12 -0400

    split gpl gear_delta now uses a piecewise crate

--- src/activity/damage_calc.rs
+++ src/activity/damage_calc.rs
@@ -1,82 +1,15 @@
 #![allow(dead_code)]
 use super::Activity;
 use crate::{enemies::EnemyType, logging, types::rs_types::DamageMods};
+use piecewise_linear::PiecewiseLinearFunction;
 use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Copy)]
-struct TableKey {
-    time: f64,
-    value: f64,
-}
-#[derive(Debug, Clone)]
-struct LinearTable {
-    table: Vec<TableKey>,
-}
-impl LinearTable {
-    fn evaluate(&self, time: f64) -> f64 {
-        if time > 0.0 {
-            return self.table[self.table.len() - 1].value;
-        }
-        let mut index = 0;
-        for i in 0..self.table.len() {
-            if self.table[i].time >= time {
-                index = i;
-                break;
-            }
-        }
-        if index == 0 {
-            return self.table[0].value;
-        }
-        if index == self.table.len() {
-            return self.table[self.table.len() - 1].value;
-        }
-        let a = self.table[index - 1];
-        let b = self.table[index];
-        let t = (time - a.time) / (b.time - a.time);
-        return a.value + (b.value - a.value) * t;
-    }
-    fn from_vecs(_times: [f64; 11], _values: [f64; 11]) -> LinearTable {
-        let mut table = Vec::new();
-        let times = _times.clone().to_vec();
-        let values = _values.clone().to_vec();
-        for i in 0..times.len() {
-            table.push(TableKey {
-                time: times[i],
-                value: values[i],
-            });
-        }
-        table.sort_by(|a, b| a.time.partial_cmp(&b.time).unwrap());
-        return LinearTable { table };
-    }
-}
 
 #[derive(Debug, Clone)]
 pub struct DifficultyData {
     name: String,
     cap: i32,
-    table: LinearTable,
+    table: PiecewiseLinearFunction<f64>,
 }
-
-const MASTER_VALUES: [f64; 11] = [
-    0.85, 0.68, 0.58, 0.535, 0.510, 0.490, 0.475, 0.46, 0.44, 0.42, 0.404,
-];
-const MASTER_TIMES: [f64; 11] = [
-    0.0, -10.0, -20.0, -30.0, -40.0, -50.0, -60.0, -70.0, -80.0, -90.0, -99.0,
-];
-
-const NORMAL_VALUES: [f64; 11] = [
-    1.0, 0.78, 0.66, 0.5914, 0.541, 0.5, 0.475, 0.46, 0.44, 0.42, 0.402,
-];
-const NORMAL_TIMES: [f64; 11] = [
-    0.0, -10.0, -20.0, -30.0, -40.0, -50.0, -60.0, -70.0, -80.0, -90.0, -99.0,
-];
-
-const RAID_VALUES: [f64; 11] = [
-    0.925, 0.73, 0.62, 0.563, 0.525, 0.495, 0.475, 0.46, 0.44, 0.42, 0.402,
-];
-const RAID_TIMES: [f64; 11] = [
-    0.0, -10.0, -20.0, -30.0, -40.0, -50.0, -60.0, -70.0, -80.0, -90.0, -99.0,
-];
 
 const WEAPON_DELTA_EXPONENT: f64 = 0.00672;
 
@@ -97,17 +30,56 @@ impl DifficultyOptions {
             DifficultyOptions::NORMAL => DifficultyData {
                 name: "Normal".to_string(),
                 cap: 50,
-                table: LinearTable::from_vecs(NORMAL_TIMES, NORMAL_VALUES),
+                table: PiecewiseLinearFunction::try_from(vec![
+                    (-99.0, 0.4018),
+                    (-90.0, 0.4200),
+                    (-80.0, 0.4400),
+                    (-70.0, 0.4600),
+                    (-60.0, 0.4750),
+                    (-50.0, 0.5000),
+                    (-40.0, 0.5405),
+                    (-30.0, 0.5915),
+                    (-20.0, 0.6600),
+                    (-10.0, 0.7800),
+                    (0.0, 1.0),
+                ])
+                .unwrap(),
             },
             DifficultyOptions::MASTER => DifficultyData {
                 name: "Master".to_string(),
                 cap: 20,
-                table: LinearTable::from_vecs(MASTER_TIMES, MASTER_VALUES),
+                table: PiecewiseLinearFunction::try_from(vec![
+                    (-99.0, 0.4018),
+                    (-90.0, 0.4200),
+                    (-80.0, 0.4400),
+                    (-70.0, 0.4600),
+                    (-60.0, 0.4750),
+                    (-50.0, 0.4900),
+                    (-40.0, 0.5100),
+                    (-30.0, 0.5350),
+                    (-20.0, 0.5800),
+                    (-10.0, 0.6800),
+                    (0.0, 0.8500),
+                ])
+                .unwrap(),
             },
             DifficultyOptions::RAID => DifficultyData {
                 name: "Raid & Dungeon".to_string(),
                 cap: 20,
-                table: LinearTable::from_vecs(RAID_TIMES, RAID_VALUES),
+                table: PiecewiseLinearFunction::try_from(vec![
+                    (-99.0, 0.4018),
+                    (-90.0, 0.4200),
+                    (-80.0, 0.4400),
+                    (-70.0, 0.4600),
+                    (-60.0, 0.4750),
+                    (-50.0, 0.4950),
+                    (-40.0, 0.5253),
+                    (-30.0, 0.5632),
+                    (-20.0, 0.62),
+                    (-10.0, 0.73),
+                    (0.0, 0.925),
+                ])
+                .unwrap(),
             },
         }
     }
@@ -127,38 +99,57 @@ pub(super) fn rpl_mult(_rpl: f64) -> f64 {
     return (1.0 + ((1.0 / 30.0) * _rpl)) / (4.0 / 3.0);
 }
 
-pub(super) fn gpl_delta(_activity: &Activity) -> f64 {
+pub(super) fn get_gear_delta_mult(_activity: &Activity) -> f64 {
     let difficulty_data: DifficultyData = _activity.difficulty.get_difficulty_data();
 
-    let rpl = _activity.rpl;
+    let epl = _activity.player.power as i32 - _activity.rpl as i32;
+
+    if epl < -99 {
+        return 0.0;
+    }
+
+    let gear_delta_mult = if epl <= 0 {
+        difficulty_data.table.y_at_x(epl as f64)
+    } else {
+        difficulty_data.table.y_at_x(0.0)
+    }
+    .unwrap();
+
+    crate::logging::log(
+        format!("gear_delta_mult: {}", gear_delta_mult).as_str(),
+        logging::LogLevel::Debug.into(),
+    );
+    gear_delta_mult
+}
+
+pub(super) fn get_wep_delta_mult(_activity: &Activity) -> f64 {
+    let difficulty_data: DifficultyData = _activity.difficulty.get_difficulty_data();
+
     let cap = if _activity.cap < difficulty_data.cap {
         _activity.cap
     } else {
         difficulty_data.cap
     };
-    let mut epl = _activity.player.pl as i32 - rpl as i32;
+
+    let epl = (_activity.player.wep_power as i32 - _activity.rpl as i32).clamp(-100, cap);
+
     if epl < -99 {
         return 0.0;
-    } else if epl > cap {
-        epl = cap;
     }
+
     let wep_delta_mult = match epl {
+        //More accurate formula from mossy
         -50..=50 => {
             (epl as f64) * 0.00683343 + (0.5 * 0.0000441279650846838 * (epl as f64).powi(2)) + 1.0
         }
+        //Formula gets messy past these values so we go to a more generic formula
         _ => std::f64::consts::E.powf(WEAPON_DELTA_EXPONENT * (epl as f64)),
     };
     crate::logging::log(
         format!("wep_delta: {}", wep_delta_mult).as_str(),
         logging::LogLevel::Debug.into(),
     );
-    let curve = difficulty_data.table;
-    let gear_delta_mult = curve.evaluate(epl as f64);
-    crate::logging::log(
-        gear_delta_mult.to_string().as_str(),
-        logging::LogLevel::Debug.into(),
-    );
-    wep_delta_mult * gear_delta_mult
+    wep_delta_mult
 }
 
 // add_remove_pve_bonuses(
@@ -179,7 +170,7 @@ pub(super) fn gpl_delta(_activity: &Activity) -> f64 {
 
 pub fn remove_pve_bonuses(_damage: f64, _combatant_mult: f64, _activity: &Activity) -> f64 {
     let rpl_mult = rpl_mult(_activity.rpl as f64);
-    let gpl_delta = gpl_delta(_activity);
+    let gpl_delta = get_gear_delta_mult(_activity);
 
     _damage / (gpl_delta * rpl_mult * _combatant_mult)
 }
