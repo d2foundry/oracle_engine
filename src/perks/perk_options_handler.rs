8f5f635c5a3ff797daa0af49840745b519aff0ed
andromeda <jen@andromeda.dev>
2023-09-06 22:42:31 -0400

    add: dead messenger

--- src/perks/perk_options_handler.rs
+++ src/perks/perk_options_handler.rs
@@ -352,6 +352,7 @@ fn hash_to_perk_option_data(_hash: u32) -> Option<PerkOptionData> {
         Perks::ColdFusion => Some(PerkOptionData::toggle()),
         Perks::BlackHole => Some(PerkOptionData::static_()),
         Perks::TemporalUnlimiter => Some(PerkOptionData::toggle()),
+        Perks::TrinaryVision => Some(PerkOptionData::static_()),
         Perks::MarksmanSights => Some(PerkOptionData::static_()),
         Perks::DexterityMod => Some(PerkOptionData::stacking(3)),
         Perks::ReserveMod => Some(PerkOptionData::stacking(3)),
