9cf39c66b96bb54c5f368bad3ddf3131d78474be
andromeda <50086718+JayAndromeda@users.noreply.github.com>
2024-03-12 17:28:50 -0400

    bungie: implement PK nerf from update 7.3.0.5 (#91)
    * bungie: implement PK nerf from update 7.3.0.5
    
    * fix: update draw and stow scalars
    
    ---------
    
    Co-authored-by: Kat Michaela <42180996+gothfemme@users.noreply.github.com>

--- src/perks/exotic_armor.rs
+++ src/perks/exotic_armor.rs
@@ -320,7 +320,7 @@ pub fn exotic_armor() {
                 let mut stats = HashMap::new();
                 if _input.calc_data.weapon_type == &WeaponType::SUBMACHINEGUN {
                     stats.insert(StatHashes::AIRBORNE.into(), 40);
-                    stats.insert(StatHashes::HANDLING.into(), 100);
+                    stats.insert(StatHashes::HANDLING.into(), 50);
                 };
                 stats
             },
@@ -333,10 +333,10 @@ pub fn exotic_armor() {
             |_input: ModifierResponseInput| -> HandlingModifierResponse {
                 if _input.calc_data.weapon_type == &WeaponType::SUBMACHINEGUN {
                     return HandlingModifierResponse {
-                        stat_add: 100,
+                        stat_add: 50,
                         ads_scale: 1.0,
-                        draw_scale: 0.6,
-                        stow_scale: 0.6,
+                        draw_scale: 0.8,
+                        stow_scale: 0.8,
                         ..Default::default()
                     };
                 }
