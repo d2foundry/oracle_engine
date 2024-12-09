9e8f6871e633cb6a765ef0809ad8a86de47500d8
andromeda <jen@andromeda.dev>
2024-12-09 09:33:15 -0500

    chore: fix wording on alloy mag to like CT

--- src/perks/perk_options_handler.rs
+++ src/perks/perk_options_handler.rs
@@ -93,7 +93,7 @@ fn hash_to_perk_option_data(_hash: u32) -> Option<PerkOptionData> {
         Perks::SwapMag => Some(PerkOptionData::static_()),
         Perks::FullChoke => Some(PerkOptionData::static_()),
         Perks::SpikeGrenades => Some(PerkOptionData::static_()),
-        Perks::AlloyMag => Some(PerkOptionData::options(["Half Mag", "Empty Mag"].to_vec())),
+        Perks::AlloyMag => Some(PerkOptionData::options(["Base", "Max Effect"].to_vec())),
         Perks::LiquidCoils => Some(PerkOptionData::static_()),
         Perks::AcceleratedCoils => Some(PerkOptionData::static_()),
         Perks::ChargetimeMW => Some(PerkOptionData::static_()),
